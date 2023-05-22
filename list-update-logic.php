8fab3e398286bbf19f0f387c840834cdc4ee12fa
typle445 <ooaungmyat1123@gamil.com>
2023-05-22 13:13:28 +0630

    update logic done

--- list-update-logic.php
+++ list-update-logic.php
@@ -1,1 +1,15 @@
 <?php
+
+require_once 'core/connection.php';
+require_once 'core/functions.php';
+
+if ($_SERVER['REQUEST_METHOD'] === "POST"){
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $money = $_POST['money'];
+    $sql = "UPDATE bankers SET name='$name',money=$money WHERE id = $id ";
+    if (mysqli_query($conn,$sql)){
+        header("Location:list-index.php");
+    }
+
+}
