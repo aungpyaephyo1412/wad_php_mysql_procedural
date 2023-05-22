fffdc92359bbc572a80a1a2c708396ff553b008c
typle445 <ooaungmyat1123@gamil.com>
2023-05-22 14:12:47 +0630

    session start & alart message add

--- list-delete.php
+++ list-delete.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 global $conn;
 require_once 'core/connection.php';
 require_once 'core/functions.php';
@@ -8,6 +8,9 @@ if ($_SERVER['REQUEST_METHOD'] === "POST"){
     $id = $_POST['id'];
     $sql = "DELETE FROM bankers WHERE id = {$id}";
     if (mysqli_query($conn,$sql)){
+        $_SESSION['status'] = [
+            'message' => 'List deleted'
+        ];
         header('Location:list-index.php');
     }
 }
--- template/footer.php
+++ template/footer.php
@@ -1,3 +1,4 @@
+<script src="<?php echo url('app.js');?>"></script>
 <script src="<?php echo url('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js')?>"></script>
 
 </body>
